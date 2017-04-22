8352e1449bee32e44bd056a4209e9609de31a9c6
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-22 18:15:35 +0430

    mk8 v5

---
+++ JAX-RS Tutorials/Basic Examples/JAX-RS -QueryParam example/src/main/java/com/mkyong/rest/UserService.java
@@ -0,0 +1,44 @@
+package com.mkyong.rest;
+
+import java.util.List;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Path("/users")
+public class UserService {
+
+	@GET
+	@Path("/query")
+	public Response getUsers(@DefaultValue("1000") @QueryParam("from") int from,
+			@DefaultValue("999")@QueryParam("to") int to,
+			@DefaultValue("name") @QueryParam("orderBy") List<String> orderBy) {
+
+		return Response
+				.status(200)
+				.entity("getUsers is called, from : " + from + ", to : " + to
+						+ ", orderBy" + orderBy.toString()).build();
+
+	}
+	
+	/*@GET
+	@Path("/query")
+	public Response getUsers(@Context UriInfo info) {
+
+		String from = info.getQueryParameters().getFirst("from");
+		String to = info.getQueryParameters().getFirst("to");
+		List<String> orderBy = info.getQueryParameters().get("orderBy");
+		
+		return Response
+				.status(200)
+				.entity("getUsers is called, from : " + from + ", to : " + to
+						+ ", orderBy" + orderBy.toString()).build();
+
+	}*/
+
+}
