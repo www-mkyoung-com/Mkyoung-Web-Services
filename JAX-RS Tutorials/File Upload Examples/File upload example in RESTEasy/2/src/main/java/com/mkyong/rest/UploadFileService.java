076b034496fedf6bc29717ca75df84323146555d
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-22 20:46:13 +0430

    mk8 v14

---
+++ JAX-RS Tutorials/File Upload Examples/File upload example in RESTEasy/2/src/main/java/com/mkyong/rest/UploadFileService.java
@@ -0,0 +1,52 @@
+package com.mkyong.rest;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+
+@Path("/file")
+public class UploadFileService {
+
+	@POST
+	@Path("/upload")
+	@Consumes("multipart/form-data")
+	public Response uploadFile(@MultipartForm FileUploadForm form) {
+
+		String fileName = "d:\\anything.png";
+		
+		try {
+			writeFile(form.getData(), fileName);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+
+		System.out.println("Done");
+
+		return Response.status(200)
+			.entity("uploadFile is called, Uploaded file name : " + fileName).build();
+
+	}
+
+	// save to somewhere
+	private void writeFile(byte[] content, String filename) throws IOException {
+
+		File file = new File(filename);
+
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+
+		FileOutputStream fop = new FileOutputStream(file);
+
+		fop.write(content);
+		fop.flush();
+		fop.close();
+
+	}
+}
